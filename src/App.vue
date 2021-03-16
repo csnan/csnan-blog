02903e9a843c3819d3913728f532ce3f15ecdfe8
蔡思楠 <csnan@caisinandeMacBook-Pro.local>
2021-03-16 16:56:15 +0800

    ADD: layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
+    <!-- <div id="jsi-flying-fish-container" class="footer-fish-container"></div> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  created () {
+    // love2d看板娘
+    window.L2Dwidget.init({
+      // 更换模型地址前后都需要修改
+      // 示例:'https://unpkg.com/live2d-widget-model-unitychan/assets/unitychan.model.json'
+      model: {
+        jsonPath: 'https://unpkg.com/live2d-widget-model-hijiki/assets/hijiki.model.json',
+        scale: 1
+      },
+      // 设置位置
+      display: {
+        position: 'left',
+        width: 100,
+        height: 180,
+        hOffset: 70,
+        vOffset: 0
+      },
+      // 手机设置
+      mobile: {
+        show: true,
+        scale: 0.5
+      },
+      // 透明度设置
+      react: {
+        opacityDefault: 1,
+        opacityOnHover: 1
+      }
+    })
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  // &::after{
+  //   content: "";
+  //   position: fixed;
+  //   top: 0;
+  //   left: 0;
+  //   width: 100%;
+  //   height: 100%;
+  //   z-index: -1;
+  //   background-repeat: no-repeat;
+  //   background-position: center;
+  //   background-size: cover;
+  //   background-image: url(https://api.imacroc.cn/acg/);
+  //   opacity: 0.06;
+  //   -webkit-filter: grayscale(100%);
+  //   -moz-filter: grayscale(100%);
+  //   -ms-filter: grayscale(100%);
+  //   -o-filter: grayscale(100%);
+  //   filter: grayscale(100%);
+  //   filter: gray;
+  // }
+
+  .footer-fish-container {
+    position: fixed;
+    bottom: -60px;
+    left: 0;
+    z-index: 99;
+    width: 100%;
+    pointer-events: none;
+  }
 }
 </style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,226 @@
+<template>
+  <div class="home-root">
+    <div class="home-display">
+      <div id="observe-star-container" class="display-star"></div>
+    </div>
+    <div class="home-main">
+      <div class="main-content">
+        <div class="content-item">
+          <div class="item-image">
+            <img src="@/assets/images/bg.jpg" alt="">
+          </div>
+          <div class="item-detail">
+            <div class="detail-date">十一月 03, 2020</div>
+            <div class="detail-title">送给自己的生日礼物</div>
+            <div class="detail-describe">我们每个人都是一个人，即使短暂的时刻有人陪在你身边，对方也终究会离去。因为生命最终的目的地就是孤寂，这种孤寂并不能靠恋爱，友谊，亲情</div>
+          </div>
+        </div>
+        <div class="content-item">
+          <div class="item-image">
+            <img src="@/assets/images/bg.jpg" alt="">
+          </div>
+          <div class="item-detail">
+            <div class="detail-date">十一月 03, 2020</div>
+            <div class="detail-title">送给自己的生日礼物</div>
+            <div class="detail-describe">我们每个人都是一个人，即使短暂的时刻有人陪在你身边，对方也终究会离去。因为生命最终的目的地就是孤寂，这种孤寂并不能靠恋爱，友谊，亲情</div>
+          </div>
+        </div>
+        <div class="content-item">
+          <div class="item-image">
+            <img src="@/assets/images/bg.jpg" alt="">
+          </div>
+          <div class="item-detail">
+            <div class="detail-date">十一月 03, 2020</div>
+            <div class="detail-title">送给自己的生日礼物</div>
+            <div class="detail-describe">我们每个人都是一个人，即使短暂的时刻有人陪在你身边，对方也终究会离去。因为生命最终的目的地就是孤寂，这种孤寂并不能靠恋爱，友谊，亲情</div>
+          </div>
+        </div>
+        <div class="content-item">
+          <div class="item-image">
+            <img src="@/assets/images/bg.jpg" alt="">
+          </div>
+          <div class="item-detail">
+            <div class="detail-date">十一月 03, 2020</div>
+            <div class="detail-title">送给自己的生日礼物</div>
+            <div class="detail-describe">我们每个人都是一个人，即使短暂的时刻有人陪在你身边，对方也终究会离去。因为生命最终的目的地就是孤寂，这种孤寂并不能靠恋爱，友谊，亲情</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.home-root {
+  &::before {
+    content: '';
+    position: fixed;
+    top: 0;
+    left: 50%;
+    z-index: -1;
+    width: 1px;
+    height: 100%;
+    background: #ebebeb;
+    transform: translateX(-50%);
+  }
+
+  .home-display {
+    overflow: hidden;
+    width: 100%;
+    height: 100vh;
+    background-image: url(~@/assets/images/bg.jpg);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+
+    .display-star {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .home-main {
+
+    .main-content {
+      width: 1200px;
+      margin: 0 auto;
+      transition: all .3s;
+
+      .content-item {
+        position: relative;
+        margin-top: 100px;
+        font-size: 0;
+
+        &:nth-child(odd) {
+          text-align: left;
+
+          .item-detail {
+            left: 660px;
+          }
+        }
+
+        &:nth-child(even) {
+          text-align: right;
+
+          .item-detail {
+            right: 660px;
+          }
+        }
+
+        .item-image {
+          display: inline-block;
+          position: relative;
+          overflow: hidden;
+          z-index: 1;
+          width: 680px;
+          height: 440px;
+          background: red;
+          border-radius: 6px;
+          cursor: pointer;
+
+          img {
+            width: 100%;
+            height: 100%;
+          }
+        }
+
+        .item-detail {
+          position: absolute;
+          top: 20px;
+          width: 500px;
+          height: 400px;
+          padding: 80px 100px 0 80px;
+          border: 1px solid #ebebeb;
+          text-align: left;
+          background-color: #ffffff;
+          border-radius: 6px;
+          box-sizing: border-box;
+
+          .detail-date {
+            font-size: 12px;
+            color: #999999;
+          }
+
+          .detail-title {
+            margin-top: 8px;
+            line-height: 25px;
+            font-size: 24px;
+          }
+
+          .detail-describe {
+            margin-top: 10px;
+            line-height: 22px;
+            font-size: 14px;
+            color: #666666;
+          }
+        }
+      }
+    }
+
+    @media screen and (max-width: 1200px) {
+      .main-content {
+        width: 900px;
+
+        .content-item {
+          &:nth-child(odd) {
+            .item-detail {
+              left: 450px;
+            }
+          }
+
+          &:nth-child(even) {
+            .item-detail {
+              right: 450px;
+            }
+          }
+
+          .item-image {
+            top: 10px;
+            width: 480px;
+            height: 310px;
+          }
+
+          .item-detail {
+            width: 420px;
+            height: 290px;
+          }
+        }
+      }
+    }
+
+    @media screen and (max-width: 900px) {
+      .main-content {
+        width: 100%;
+
+        .content-item {
+          .item-image {
+            display: block;
+            width: 100%;
+            height: auto;
+            border-radius: 0;
+          }
+
+          .item-detail {
+            position: static;
+            width: 100%;
+            height: auto;
+            padding: 40px 20px;
+            border: none;
+            border-bottom: 1px solid #ebebeb;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
